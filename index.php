6b25992d6953dd2b2de011ef71d1ed545031d410
Rosa <costurezati@gmail.com>
2025-04-28 19:27:44 -0300

    .

--- index.php
+++ index.php
@@ -51,8 +51,7 @@ $produtos = listarProdutos();
     </div>
 </section>
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-    <script src="./resources/js/index.js"></script>
+
     
 </body>
 </html>
