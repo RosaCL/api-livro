45ac0f8efbbea583f1740f62dce74d819825f4b1
Rosa <costurezati@gmail.com>
2025-04-30 18:05:50 -0300

    .

--- app/index.php
+++ app/index.php
@@ -1,4 +1,4 @@
-<?php
+?php
 require_once('inc/config.php');
 require_once('inc/api_functions.php')
 ?>
@@ -15,42 +15,65 @@ require_once('inc/api_functions.php')
 <body>
 <header class="header">    
         <a href="#" class="logo">Filhas de D.Helena</a>
-        <nav class="navbar">
-        <a href="./create.php">Adicionar produtos</a>        
-        <a href="./index.php">Todos os produtos</a>          
+        <nav class="navbar">             
+        <a href="#product">Todos os produtos</a>          
         </nav>
         <div id="menu-btn" class="fas fa-bars"></div>  
 </header>
-<section class="product">
+<section class="product" id="product">
     <div class="box">
     <h1 class="heading"> Produtos Cadastrados</h1>
     <table>
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Autor</th>
-            <th>Gênero</th>
-            <th>Preço</th>
-            <th>Quantidade</th>
-        </tr>
-        <?php
-        $results = api_request('get_all_products', 'GET');?>
-        <?php foreach($results['data']['results'] as $product) :?>
             <tr>
-            <td><?= $product['id_product'] ?></td>
-            <td><?= $product['nome'] ?></td>
-            <td><?= $product['autor'] ?></td>
-            <td><?= $product['genero'] ?></td>
-            <td>R$ <?= $product['preco']?></td>
-            <td><?= $producto['quantidade']?></td>
-            <td>
-                <a class="btn" href="update.php?id=<?= $product['id'] ?>">Editar</a> 
-                <a class="delete-btn" href="delete.php?id=<?= $product['id'] ?>" onclick="return confirm('Deseja deletar?')">Deletar</a>
-            </td>
+                <th>ID</th>
+                <th>Produto</th>
+                <th>Autor</th>
+                <th>Gênero</th>
+                <th>Preço</th>
+                <th>Quantidade</th>                
+                <th>Criação</th>
+            </tr>
+            <?php
+            $results = api_request('get_all_products', 'GET');?>
+            <?php foreach($results['data']['results'] as $product) :?>
+            <tr>
+                <td><?=$product['id_produto']?></td>
+                <td><?=$product['nome']?></td>
+                <td><?=$product['autor']?></td>
+                <td><?=$product['genero']?></td>
+                <td>R$<?=$product['preco']?></td>
+                <td><?=$product['quantidade']?></td>
+                <td><?=$product['created_at']?></td>
             </tr>
             <?php endforeach; ?>
-    </table>
+
+        </table>
     </div>
 </section> 
+<section class="clientes" id="clientes">
+    <div class="box">
+        <h1 class="heading">Clientes Cadastrados</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Criação</th>
+            </tr>
+            <?php $results = api_request('get_all_clients', 'GET');?>
+            <?php foreach($results['data']['results'] as $client) :?>
+            <tr>
+                <td><?=$client['id_client']?></td>
+                <td><?=$client['name']?></td>
+                <td><?=$client['email']?></td>
+                <td><?=$client['tel']?></td>
+                <td><?=$client['created_at']?></td>
+            </tr>
+            <?php endforeach; ?>
+
+        </table>
+    </div>
+</section>
 </body>
 </html>
